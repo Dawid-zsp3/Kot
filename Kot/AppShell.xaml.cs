08a30b9d4c3a077261d3d195bbd0d0ae3e23b903
Dawid-zsp3 <uT_3772@zsp3zamosc.pl>
2024-11-07 09:33:20 +0100

    Dodaj pliki projektów.

---
+++ Kot/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Kot
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kot/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Windows.Web.Http.Headers;
+using System;
+using System.Net.Http;
+
+namespace Kot
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            imgCat.Source = new UriImageSource
+            {
+               Uri = new Uri("https://cataas.com/cat?unique=")
+            };
+
+        }
+        private void OnButtonClicked1(object sender, EventArgs e)
+        {
+            imgCat1.Source = new UriImageSource
+            {
+                Uri = new Uri("https://cataas.com/cat?unique=")
+            };
+
+        }
+        private void OnButtonClicked2(object sender, EventArgs e)
+        {
+            string kot = koty.Text;
+            imgCat2.Source = new UriImageSource
+            {
+                Uri = new Uri("https://cataas.com/cat/says" + kot)
+            };
+
+        }
+
+
+    }
+
+}
